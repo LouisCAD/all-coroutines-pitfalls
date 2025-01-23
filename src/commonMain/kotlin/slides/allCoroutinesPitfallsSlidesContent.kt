7d430966ebf479e06400c4cf864759125d31833c
Louis CAD <louis.cognault@gmail.com>
2025-01-23 10:12:20 +0100

    WIP

--- src/commonMain/kotlin/slides/allCoroutinesPitfallsSlidesContent.kt
+++ src/commonMain/kotlin/slides/allCoroutinesPitfallsSlidesContent.kt
@@ -6,7 +6,7 @@ fun allCoroutinesPitfallsSlidesContent() = slides {
     }
     "Let's talk definitions".slidesGroup {
         subtitle("To make sure we're on the same page")
-        "What is a coroutine?".bullets {
+        "What is a coroutine?".slide(Bullets) {
             "co-routine = cooperative routine" {
                 "routine : some code (i.e. a function) being executed"()
             }
@@ -14,18 +14,18 @@ fun allCoroutinesPitfallsSlidesContent() = slides {
             "It's a suspendable function, while it's being run"()
         }
         comparison {
-            "routine".bullets {
+            "routine".slide(Bullets) {
                 "The calling thread is blocked/monopolized until it's done" {
                     "Callbacks are required for asynchronous operations"()
                 }
             }
-            "coroutine".bullets {
+            "coroutine".slide(Bullets) {
                 "Can suspend its execution to let other ones run, and resume later on" {
                     "Callbacks are *not* needed"()
                 }
             }
         }
-        "Why do we want to avoid callbacks?".bullets {
+        "Why do we want to avoid callbacks?".slide(Bullets) {
             "They make everything more complicated!"()
             "Logic is spread all over the place" {
                 "The code can't be read linearly"()
@@ -38,10 +38,43 @@ fun allCoroutinesPitfallsSlidesContent() = slides {
             "Handling errors is even harder (a nightmare, to put it lightly)"()
         }
     }
-    "Why use coroutines? My reasons".bullets(mode = Bullets.Numbers) {
+    "Why use coroutines? My reasons".slide(Bullets.Numbers) {
         "Avoid callbacks… and all their problems !"()
         "Get back to the simplicity of sequential code"()
         "Make saving resources easier"()
         "Open new ways of programming"()
+    }
+    "Coroutines are a _fundamentally_ new way to do all things asynchronous".slide(BigFact)
+    kindsOfPitfalls()
+    "Basics in 5 minutes".slidesGroup(smallTitle = "Basics") {
+        subtitle("Before showing you all the wrong ways, let's try an attempt at showing you the right way")
+        "suspend functions".slidesGroup(Bullets) {
+            "What they look like" {
+                "They have the **suspend** modifier"()
+                //TODO: have a bunch of suspend function signatures pop-up, with redacted bodies (or not, for very short ones?)
+                "They complete only once their \"work\" is:" {
+                    "done"()
+                    "failed"()
+                    "cancelled"(sideLabel = "More on that later…")
+                }
+            }
+            "How they work" {
+                "They are compiled to normal functions…"()
+                "They are compiled to normal functions…"()
+            }
+            ""
+        }
+        "suspend funs' hidden power" {
+            "cancellation"()
+        }
+    }
+}
+
+fun SlidesBuilder.kindsOfPitfalls() {
+    "4 kinds of pitfalls".slide(Bullets.Abc) {
+        "You don't know **what you want to do** exactly"()
+        "You don't really know **what you're using**"()
+        "You didn't rethrow CancellationException"(sideLabel = "most common one!")
+        "You forgot something while doing a minor refactoring"()
     }
 }
