3db0db61b3b8e47db0b86b7cac988617a640f554
Louis CAD <louis.cognault@infomaniak.com>
2025-03-27 17:19:19 +0100

    WIP: Add snippets for structured concurrency

--- src/commonMain/kotlin/content/pitfalls/BreakingStructuredConcurrency.kt
+++ src/commonMain/kotlin/content/pitfalls/BreakingStructuredConcurrency.kt
@@ -1,10 +1,43 @@
 package content.pitfalls
 
 import dsl.SlidesBuilder
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 
 fun SlidesBuilder.breakingStructuredConcurrency() {
     "Jobless CoroutineScope".slide {
         ""()
     }
     "Overwrite".slide()
+}
+
+private suspend fun doStuff() = coroutineScope {
+    launch {  }.children.flatMap { it.children }.flatMap { it.children }
+    val someScope = this
+    someScope.launch {
+        someSuspendFunction()
+    }
+    someScope.launch {
+        launch {
+            anotherThingToDoConcurrently()
+        }
+        someSuspendFunction()
+    }
+    coroutineScope {
+        launch {
+            someSuspendFunction()
+        }
+        launch {
+            someSuspendFunction()
+        }
+    }
+}
+
+suspend fun anotherThingToDoConcurrently() {
+    TODO("Not yet implemented")
+}
+
+private suspend fun someSuspendFunction() {
+    yield()
 }
